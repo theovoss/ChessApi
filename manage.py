befdd9bc1a6bc71bc439ac11c7a0ad1f3540aeaa
Theo Voss <theo.voss973@gmail.com>
2015-09-14 19:28:33 -0400

    fix manage.py to actually run the commands it's supposed to.

--- manage.py
+++ manage.py
@@ -9,6 +9,7 @@ from api.database import db
 from api.app import create_app
 from api.settings import ProdConfig, DevConfig
 
+app = None
 if os.environ.get("chess_api_ENV") == 'prod':
     app = create_app(ProdConfig)
 else:
@@ -27,16 +28,17 @@ def _make_context():
     return {'app': app, 'db': db}
 
 
+@manager.command
 def test():
     """Run the tests."""
     import pytest
     exit_code = pytest.main([TEST_PATH, '--verbose'])
     return exit_code
 
-manager.add_command('test', test())
+# manager.add_command('test', test)
 manager.add_command('server', Server())
 manager.add_command('shell', Shell(make_context=_make_context))
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
